48106b168eccd6a8c700a3062791798c60ceae60
hanzhichao <hanzhichao@kingsoft.com>
2019-11-22 11:39:18 +0800

    1.commit

--- controller/wcbz/situation.js
+++ controller/wcbz/situation.js
@@ -510,12 +510,22 @@ class Situation extends BaseComponent{
         const ensure = await ensureModel.find(ensuredate);
 
         // 当天保障计划飞机编号的数组
-        let ensureArray = ensure.length ? ensure[0].filed3.map(v => {
-            if (!v.airplane && v.plan) {
-                return planArray
-            }
-            return v.airplane.map(vv =>  vv.code)
-        }) : [];
+        // let ensureArray = ensure.length ? ensure[0].filed3.map(v => {
+        //     if (!v.airplane && v.plan) {
+        //         return planArray
+        //     }
+        //     return v.airplane.map(vv =>  vv.code)
+        // }) : [];
+
+        let ensureArray = [];
+        ensure.length ? ensure.forEach(vv => {
+            ensureArray = vv.filed3.map(v => {
+                if (!v.airplane && v.plan) {
+                    return planArray
+                }
+                return v.airplane.map(vv =>  vv.code)
+            })
+        }) : ensureArray = [];
 
         const newAirArray = [].concat.apply([], ensureArray);
 
